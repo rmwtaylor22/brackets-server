99b2c412abd2fd119b449d788158fda4030eefc0
rwhite <rose_white@taylor.edu>
2022-03-10 14:12:25 -0500

    fixed server errors

--- server/server.js
+++ server/server.js
@@ -1,99 +1,19 @@
-// aquire express
 const express = require("express");
-
-// initialize the application
 const app = express();
-
-// cors is middleware, helps communication between the cross origin
 const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
 app.use(cors());
-
-// Stringifies the data coming in from the requests
 app.use(express.json());
-
-// add mongoose
-const mongoose = require("mongoose");
-
-const mongoAtlasUri =
-  "mongodb+srv://mern:mongodb@brackets.l3ri0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
-try {
-  // Connect to the MongoDB cluster
-  mongoose.connect(
-    mongoAtlasUri,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => console.log(" Mongoose is connected")
-  );
-} catch (e) {
-  console.log("could not connect");
-}
-
-const dbConnection = mongoose.connection;
-dbConnection.on("error", (err) => console.log(`Connection error ${err}`));
-dbConnection.once("open", () => console.log("Connected to DB!"));
-
-// import user model
-const User = require("./models/user.model");
-
-
-
-
-// call some routes
-
-// register
-app.post("/api/user/register", async (req, res) => {
-  console.log(req.body);
-  try {
-    await User.create({
-      name: req.body.name,
-      username: req.body.username,
-      password: req.body.password,
-    });
-    res.json({ status: "ok" });
-  } catch (err) {
-    console.log(err);
-    res.json({ status: "error", error: "Duplicate username" });
-  }
-});
-
-// login
-app.post("/api/user/login", async (req, res) => {
-  const user = await User.findOne({
-    username: req.body.username,
-    password: req.body.password,
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
+ 
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
   });
-
-  if (user) {
-    const token = 'secret123';
-    return res.json({ status: "ok", user: token });
-  } else {
-    return res.json({ status: "error", user: false });
-  }
-});
-
-// get users and scores
-/* app.get("/api/user/leaderboard", async (req, res) => {
-  const user = await User.find({})
-    .toArray(function (err, result) {
-      if (err) throw err;
-      res.json(result);
-      console.log(result)
-    });
-}); */
-// This section will help you get a list of all the users.
-routes.route("/user").get(function (req, res) {
-  let db_connect = dbo.getDb();
-  db_connect
-    .collection("users")
-    .find({})
-    .toArray(function (err, result) {
-      if (err) throw err;
-      res.json(result);
-    });
-});
-
-
-// start the server
-app.listen(1337, () => {
-  console.log("Server started on 1337");
+  console.log(`Server is running on port: ${port}`);
 });
