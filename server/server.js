e5b444b59b6e08d8e4515be434a6b39edc057718
rwhite <rose_white@taylor.edu>
2022-03-13 20:15:47 -0400

    Changed the server to be able to server from anywhere

--- server/server.js
+++ server/server.js
@@ -9,11 +9,5 @@ app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
  
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+app.listen(process.env.PORT || 5000);
+app.get('/', (req, res) => { res.send('Hello from Express!')})
